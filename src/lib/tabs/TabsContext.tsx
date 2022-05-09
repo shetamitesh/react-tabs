bc3e938803a0bc7f3856753ee6acbff336b590bb
Mitesh <mitesh2vuesolution.com>
2022-05-09 18:05:31 +0530

    added onclick

--- src/lib/tabs/TabsContext.tsx
+++ src/lib/tabs/TabsContext.tsx
@@ -15,6 +15,7 @@ class TabProvider extends React.Component {
   props: {
     activeTab: any;
     children: any;
+    onClick: any;
   };
 
   state = {
@@ -32,6 +33,7 @@ class TabProvider extends React.Component {
           tabs: prevState.tabs.concat(tab)
         };
       });
+      this.props.onClick()
     }
   };
 
